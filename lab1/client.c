cbad6893e84b57453b8a059807358fef5bf03d5f
fumycat <fumycat@gmail.com>
2021-02-18 03:41:20 +0000

    vse rabotaet

---
+++ lab1/client.c
@@ -0,0 +1,41 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <strings.h>
+#include <netdb.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define handle_error(msg) \
+    do { perror(msg); exit(1); } while (0)
+
+
+int main(int argc, char const *argv[])
+{
+	for (int x = 0; x < 5; x++) {
+
+		struct hostent *hp, *gethostbyname();
+
+	    int s = socket(AF_INET, SOCK_STREAM, 0);
+	    if (s == -1) handle_error("socket");
+
+	    struct sockaddr_in ss;
+	    bzero((char*)&ss, sizeof(ss));
+	    ss.sin_family = AF_INET;
+	    hp = gethostbyname(argv[1]);
+	    bcopy(hp -> h_addr, &ss.sin_addr, hp -> h_length);
+	    ss.sin_port = htons(atoi(argv[2]));
+	    int len = sizeof(ss);
+
+		if (connect(s, (struct sockaddr *)&ss, sizeof(struct sockaddr_in)) == -1) handle_error("connect");
+
+	    write(s, argv[3], 1);
+
+		close(s);
+		sleep(atoi(argv[3]));
+    }
+    return 0;
+}
--- lab1/server.c
+++ lab1/server.c
@@ -1,10 +1,13 @@
 #include <stdio.h> // printf
 #include <stdlib.h> // exit
 #include <unistd.h> // close
+
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#define BUFF_SIZE 32
 
 #define handle_error(msg) \
     do { perror(msg); exit(1); } while (0)
@@ -12,26 +15,38 @@
 
 int main(int argc, char const *argv[])
 {
+    int sock_clnt;
+
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     int flag = 1;
     setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(int));
         
     struct sockaddr_in ss;
     ss.sin_family = AF_INET;
-    ss.sin_addr.s_addr = htons(INADDR_ANY);
+    ss.sin_addr.s_addr = inet_addr("127.0.0.1"); // htons(INADDR_ANY);
 
     if (bind(sock, (struct sockaddr *)&ss, sizeof(struct sockaddr_in)) == -1) handle_error("bind");
 
-    getsockname(sock, (struct sockaddr *)&ss, NULL);
+    int len = sizeof(ss);
+    getsockname(sock, (struct sockaddr *)&ss, &len);
 
     printf("port is %d\n", ntohs(ss.sin_port));
 
     if (listen(sock, 5) == -1) handle_error("listen");
 
-    int childpid = fork();
-    int ogpid = getpid();
+    for (;;) {
+        if ((sock_clnt = accept(sock, NULL, 0)) < 0) {
+            printf("err\n");
+        }
+        int bred;
+        char buff[BUFF_SIZE];
 
-    printf("%d and %d\n", getpid, childpid);
+        while ((bred = read(sock_clnt, buff, BUFF_SIZE)) > 0) {
+            write(STDIN_FILENO, buff, bred);
+        }
+        write(STDIN_FILENO, "\n", 1);
+        close(sock_clnt);
+    }
 
     close(sock);
 
